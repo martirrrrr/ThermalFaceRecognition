1abc372c40c5f9fd4d98e895da6c27d6a9e8f95e
Martina <114102344+martirrrrr@users.noreply.github.com>
2025-05-27 09:53:24 +0200

    Update summary.py

--- summary.py
+++ summary.py
@@ -6,23 +6,23 @@ from torchvision import datasets, transforms
 # Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Trasformazioni (devono essere uguali a quelle usate nel training)
+# Trasformations (as for train and test)
 transform_test = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),
     transforms.Resize((112, 112)),
     transforms.ToTensor(),
 ])
 
-# Carica dataset solo per ottenere il numero di classi
+# Load detaset
 train_ds = datasets.ImageFolder('dataset_merged_split/train', transform=transform_test)
 
-# Istanzia e carica modello
+# Load model
 model = CustomResNet(num_classes=len(train_ds.classes)).to(device)
 model.load_state_dict(torch.load('best_thermal_model2.pth', map_location=device))
 
-# Stampa riepilogo
+# Print summary
 summary(model, input_size=(1, 112, 112))
 
 
 from torchinfo import summary
-summary(model, input_size=(32, 1, 112, 112))  # batch_size=32 opzionale
+summary(model, input_size=(32, 1, 112, 112))
